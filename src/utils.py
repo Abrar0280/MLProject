114be0c39e68bb300e74123eb2889df301ca9e8b
Abrar 0280 <mohamedabrar028@gmail.com>
2023-10-16 21:45:41 +0530

    Hyper Parameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import dill
 from src.exception import CustomException
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
@@ -19,7 +19,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train,X_test,y_test, models):
+def evaluate_model(X_train, y_train,X_test,y_test, models,param):
     try:
         # X_train, X_test, y_train,y_test=train_test_split(
         #     X,y,test_size=0.2, random_state=42
@@ -28,15 +28,15 @@ def evaluate_model(X_train, y_train,X_test,y_test, models):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            # para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            # gs = GridSearchCV(model,para,cv=3)
-            # gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            # model.set_params(**gs.best_params_)
-            # model.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-            model.fit(X_train, y_train)  # Train model
+            # model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
