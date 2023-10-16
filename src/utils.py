201f5c8243125219d51de293158c13784d399439
Abrar 0280 <mohamedabrar028@gmail.com>
2023-10-17 00:08:31 +0530

    Predict Pipeline

--- src/utils.py
+++ src/utils.py
@@ -54,7 +54,7 @@ def evaluate_model(X_train, y_train,X_test,y_test, models,param):
     except Exception as e:
         raise CustomException(e, sys)
     
-def load_object(file_path):
+def load_object(file_path): # Load object
     try:
         with open(file_path, "rb") as file_obj:
             return pickle.load(file_obj)
